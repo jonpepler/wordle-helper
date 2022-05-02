da048642cfd1bef608c7e65e296d36d4b41a26dd
Jon Pepler <jonathan.pepler@gmail.com>
2022-05-02 23:43:16 +0100

    clean up cmd interface

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
 import https from 'https'
 
-import { question } from 'readline-sync'
+import { keyIn, keyInSelect } from 'readline-sync'
 
 import { Search } from './Search'
 import { getGuessFromString } from './util'
 
+const grey = (letter: string): string => `\u001b[40m${letter}\u001b[0m`
+const yellow = (letter: string): string => `\u001b[43;1m${letter}\u001b[0m`
+const green = (letter: string): string => `\u001b[42;1m${letter}\u001b[0m`
+const colourGuessString = (guess: string): string =>
+  guess
+    .toUpperCase()
+    .split(' ')
+    .slice(0, -1)
+    .map((letter) =>
+      letter[1] === '0'
+        ? grey(letter[0])
+        : letter[1] === '1'
+        ? yellow(letter[0])
+        : green(letter[0])
+    )
+    .join('')
+
 const runSearch = (solutions: string[]): void => {
   const search = new Search(solutions)
-  const loop = (): void => {
-    const input = question('enter guess: ')
+  const loop = (wordList: string[] = []): void => {
+    let input = ''
+    while (input.length < 15) {
+      const colour = keyInSelect(['⬛', '🟨', '🟩', 'X'], 'colour? ')
+      if (colour === 3) process.exit()
+      const letterInput = keyIn('letter? ', { limit: '$<a-z>' })
+      input += `${String(letterInput)}${String(colour)} `
+      console.clear()
+      console.log()
+      wordList.forEach((word) => console.log(word))
+      console.log(colourGuessString(input))
+    }
     search.addGuess(getGuessFromString(input))
     search.filter()
     search.report()
 
-    if (search.possibleSolutions.length > 1) loop()
+    if (search.possibleSolutions.length > 1) loop([...wordList, colourGuessString(input)])
   }
 
   loop()
